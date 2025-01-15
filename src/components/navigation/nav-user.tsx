312d978a4a4bf8fedf9c074ea3f9d244796168f9
Jeffin <jeffinj787@gmail.com>
2025-01-15 11:29:16 +0000

    fix(sidebar): show user info in sidebar

--- src/components/navigation/nav-user.tsx
+++ src/components/navigation/nav-user.tsx
@@ -29,6 +29,7 @@ import {
     SidebarMenuItem,
     useSidebar,
 } from "@/components/ui/sidebar"
+import { useAuth } from "@/providers/auth.context"
 
 export function NavUser({
     user,
@@ -40,6 +41,7 @@ export function NavUser({
     }
 }) {
     const { isMobile } = useSidebar()
+    const { user: signedInUser } = useAuth()
 
     return (
         <SidebarMenu>
@@ -51,12 +53,12 @@ export function NavUser({
                             className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                         >
                             <Avatar className="h-8 w-8 rounded-lg">
-                                <AvatarImage src={user.avatar} alt={user.name} />
+                                <AvatarImage src={user.avatar} alt={signedInUser?.firstName} />
                                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                             </Avatar>
                             <div className="grid flex-1 text-left text-sm leading-tight">
-                                <span className="truncate font-semibold">{user.name}</span>
-                                <span className="truncate text-xs">{user.email}</span>
+                                <span className="truncate font-semibold">{signedInUser?.firstName}</span>
+                                <span className="truncate text-xs">{signedInUser?.email}</span>
                             </div>
                             <ChevronsUpDown className="ml-auto size-4" />
                         </SidebarMenuButton>
