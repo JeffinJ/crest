cdc1c60680f8ef5b00f3097a3b463a123ef810af
Jeffin <jeffinj787@gmail.com>
2025-01-15 18:39:06 +0000

    feat(home): hero card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { AnimatedHeroCard } from "@/components/hero-card-animated";
+
 export default function Home() {
   return (
-    <div className="p-5">
-      <h1>Home page</h1>
+    <div className="flex flex-row h-full items-center justify-center">
+      <div className="h-full flex flex-col gap-y-5 items-start justify-center">
+        <div className="flex flex-col gap-y-2">
+          <AnimatedHeroCard />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/hero-card-animated.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import { SupportedPlatformsPin } from "./supported-accounts";
+import Image from "next/image";
+import emerald from "../../public/assets/gem.png";
+
+export function AnimatedHeroCard() {
+    return (
+        <div className="h-[40rem] w-full flex items-center justify-center ">
+            <SupportedPlatformsPin>
+                <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                    <div className="font-semibold text-4xl text-emerald-500 font-RubikVinyl text-center">
+                        CREST
+                    </div>
+                    <div className="text-center text-sm py-1">
+                        Your global profile
+                    </div>
+                    <div className="text-base !m-0 !p-0 font-normal flex items-center justify-center">
+                        <Image
+                            src={emerald}
+                            alt="emerald"
+                            width={250}
+                            height={250}
+                            priority
+                            className=""
+                        />
+                    </div>
+                    <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+                </div>
+            </SupportedPlatformsPin>
+        </div>
+    );
+}
