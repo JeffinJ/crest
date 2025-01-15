98014d71749ef4a664bfcb6d0a989de088f5c278
Jeffin <jeffinj787@gmail.com>
2025-01-15 12:15:49 +0000

    feat(links): add youtube account

--- src/app/(secured)/admin/links/page.tsx
+++ src/app/(secured)/admin/links/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function ManageLinksPage() {
     return (
         <div>
             <h1>Manage Links</h1>
+            <Button>
+                Connect your Youtube account
+            </Button>
         </div>
     )
 };
