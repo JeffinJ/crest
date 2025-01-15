f155a103cf01f5e4b116597af76944d0ebd65e23
Jeffin <jeffinj787@gmail.com>
2025-01-15 19:40:29 +0000

    feat: add spinner to fullscreen loader

--- src/components/fullscreen-loader.tsx
+++ src/components/fullscreen-loader.tsx
@@ -1,7 +1,12 @@
+import spinner from "../../public/assets/spinner.svg";
+import Image from "next/image";
+
 export default function FullScreenLoader() {
     return (
         <div className="h-screen w-full bg-transparent flex items-center justify-center">
-            Loading
+            <Image src={spinner} alt="spinner" width={50} height={50}
+                priority
+                className="w-20 h-20" />
         </div>
     )
 };
