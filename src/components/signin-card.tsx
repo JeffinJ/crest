eb39b6356b442c4c63b6dde8354fe04976086473
Jeffin <jeffinj787@gmail.com>
2025-01-20 15:08:20 +0000

    fix(signin-page): show loggedin user name in signin card

--- src/components/signin-card.tsx
+++ src/components/signin-card.tsx
@@ -5,11 +5,19 @@ import {
     IconBrandGoogle,
 } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/providers/auth.context";
+import { StepForward } from "lucide-react";
+import { ButtonWithGradient } from "./ui/button-with-gradient";
 
 export function SignInCard() {
     const router = useRouter();
+    const { user, jwt } = useAuth();
     const handleSignIn = () => {
         router.push(`${process.env.NEXT_PUBLIC_CREST_AUTH_CENTER_URL}/auth/google/signin`);
+    };
+
+    const continueAsLoggedInUser = () => {
+        router.push("/admin");
     };
 
     return (
@@ -22,6 +30,23 @@ export function SignInCard() {
             </p>
             <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
             <div className="flex flex-col space-y-4">
+                {jwt && user && (
+                    <ButtonWithGradient
+                        className=" relative group/btn flex items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                        type="button"
+                        onClick={continueAsLoggedInUser}
+                        gradientColors={{
+                            via1: "yellow-500",
+                            via2: "emerald-400"
+                        }}
+                    >
+                        <StepForward className="h-5 w-5 text-neutral-800 dark:text-yellow-300" />
+                        <span className="text-neutral-700 dark:text-yellow-300 text-sm">
+                            Continue as {user.firstName}
+                        </span>
+                        <BottomGradient />
+                    </ButtonWithGradient>
+                )}
                 <button
                     className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
                     type="button"
